e4424ae99a91318b3e6ad09bd5870e227f9ab750
Abhis9399 <abhidahi67@gmail.com>
2024-07-13 07:25:37 +0530

    vercel link

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { createServer } = require('http');
 const next = require('next');
+const cors = require('cors'); // Import CORS
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -8,9 +9,17 @@ const handle = app.getRequestHandler();
 const port = 3001;
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
+  const server = createServer((req, res) => {
+    // Use CORS for all requests
+    cors({
+      origin: 'https://unitus-admin-1cjcvqq51-abhishek-dahiwals-projects.vercel.app/', // Replace with your Vercel deployment URL
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    })(req, res, () => {
+      handle(req, res);
+    });
+  });
+
+  server.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
